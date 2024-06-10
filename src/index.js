59ab5472bede06742e7f6505ba3156c361e9b72a
luizandremiranda <luizmiranda.lasm@gmail.com>
2024-06-10 19:16:40 -0300

    NivelHeroi

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
-let Nome = ""
+let nome = ""
 let xp = 0
-let classificação = ""
+let classificacao = ""
 
-nome = prompt ("Digite o nome do héroi")
-xp = parseInt(prompt ("Quanto de experiencia o herói tem?"));
+nome = prompt("Digite o nome do héroi")
+xp = parseInt(prompt("Quanto de experiencia o herói tem?"));
 if (xp < 1000 && xp <= 0) {
-classificação = Bronze
+classificacao = Bronze
 }
 else if (xp >=1001 && xp <=5000){
-    classificação = "Prata"
+    classificacao = "Prata"
 }
 else if (xp >=5001 && xp <=7000){
-    classificaçãocl
-     = "Ouro"
+    classificacao = "Ouro"
 }
 else if (xp >= 7001 && xp <=8000){
-    classificação = "Platina"
+    classificacao = "Platina"
 }
 else if (xp >=8001 && xp <= 9000){
-    classificação = "Ascendente"
+    classificacao = "Ascendente"
 }
 else if (xp >= 9001 && xp <= 10000){
-    classificação = "Imortal"
+    classificacao = "Imortal"
 }
 else if (xp>= 10001){
-    classificação = Radiante
+    classificacao = "Radiante"
 }
-
-else {"O herói " + nome + "está fora da classificação"}
-console.log (nome + " está no nivel " + classificação)
-
+console.log ("O herói " + nome + " está na classificação " + classificacao)
